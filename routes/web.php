f1a5dc45d2d9f36e13c5f40888c14e5e1e7436f4
yiir27 <camp.yui.c@gmail.com>
2023-10-26 02:09:02 +0900

    バックエンド設定

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ListsController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
@@ -33,6 +34,11 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/lists', [ListsController::class, 'index']) -> name('lists.index');
+    Route::post('/lists/store', [ListsController::class, 'store']) -> name('lists.store');
+    Route::put('/lists/update/{id}',[ListsController::class, 'update'])-> name('lists.update');
+    Route::delete('/lists/destroy/{id}',[ListsController::class,'destroy']) -> name('lists.destroy');
 });
 
 require __DIR__.'/auth.php';
